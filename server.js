ebccfe84f5f7bc69f397693298adc925a5cf1062
wa1ead <waliddraa2@gmail.com>
2024-10-30 20:08:23 +0100

    generating get route

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const User = require("./models/User");
 require("dotenv").config();
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(console.log("CONNECTED SUCCESSFULLY"))
-  .catch((err) => {
-    console.error(err);
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("DATABASR CONNECTED SUCCESSFULLY");
+  app.listen(3000, () => {
+    console.log("SERVER STARTED SUCCESSFULLY");
   });
+});
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
