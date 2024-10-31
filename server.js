178e3948fb0ded5f56ab787256e726749025c578
wa1ead <waliddraa2@gmail.com>
2024-10-31 17:52:48 +0100

    using try/catch blocks

--- server.js
+++ server.js
@@ -13,28 +13,44 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 app.use(express.json());
 
 app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.post("/users/new", async (req, res) => {
-  const user = await new User({ name: req.body.name, job: req.body.job });
-  console.log(req.body);
-  await user.save();
-  res.send(user);
+  try {
+    const user = await new User({ name: req.body.name, job: req.body.job });
+    console.log(req.body);
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.put("/user/:id", async (req, res) => {
-  const user = await User.findOneAndUpdate(
-    { _id: req.params.id },
-    { name: req.body.name, job: req.body.job },
-    { new: true }
-  );
-  await user.save();
-  res.send(user);
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { name: req.body.name, job: req.body.job },
+      { new: true }
+    );
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 app.delete("/user/:id", async (req, res) => {
-  const user = await User.findByIdAndDelete(req.params.id);
-  res.send(user);
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.send(user);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
